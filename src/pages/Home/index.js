c7134e599c7bc285e6cc7b29bd34ae7ab62ecb4a
WesleyHiromitsu <wes.hiro@gmail.com>
2020-04-16 19:23:08 -0300

    Added Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,101 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-2-masculino/46/D16-4274-246/D16-4274-246_detalhe1.jpg?ts=1586370047?ims=280x280"
+          alt="Tenis"
+        />
+        <strong>Tenis muito Legal</strong>
+        <span>R$ 129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-2-masculino/46/D16-4274-246/D16-4274-246_detalhe1.jpg?ts=1586370047?ims=280x280"
+          alt="Tenis"
+        />
+        <strong>Tenis muito Legal</strong>
+        <span>R$ 129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-2-masculino/46/D16-4274-246/D16-4274-246_detalhe1.jpg?ts=1586370047?ims=280x280"
+          alt="Tenis"
+        />
+        <strong>Tenis muito Legal</strong>
+        <span>R$ 129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-2-masculino/46/D16-4274-246/D16-4274-246_detalhe1.jpg?ts=1586370047?ims=280x280"
+          alt="Tenis"
+        />
+        <strong>Tenis muito Legal</strong>
+        <span>R$ 129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-2-masculino/46/D16-4274-246/D16-4274-246_detalhe1.jpg?ts=1586370047?ims=280x280"
+          alt="Tenis"
+        />
+        <strong>Tenis muito Legal</strong>
+        <span>R$ 129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-2-masculino/46/D16-4274-246/D16-4274-246_detalhe1.jpg?ts=1586370047?ims=280x280"
+          alt="Tenis"
+        />
+        <strong>Tenis muito Legal</strong>
+        <span>R$ 129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
