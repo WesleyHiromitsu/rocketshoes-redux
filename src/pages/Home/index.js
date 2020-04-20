affd644f8e2d6dd5394aa2f7303be496d9548f6b
WesleyHiromitsu <wes.hiro@gmail.com>
2020-04-20 12:14:06 -0300

    Added product from jsonserver

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,101 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-2-masculino/46/D16-4274-246/D16-4274-246_detalhe1.jpg?ts=1586370047?ims=280x280"
-          alt="Tenis"
-        />
-        <strong>Tenis muito Legal</strong>
-        <span>R$ 129,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-2-masculino/46/D16-4274-246/D16-4274-246_detalhe1.jpg?ts=1586370047?ims=280x280"
-          alt="Tenis"
-        />
-        <strong>Tenis muito Legal</strong>
-        <span>R$ 129,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-2-masculino/46/D16-4274-246/D16-4274-246_detalhe1.jpg?ts=1586370047?ims=280x280"
-          alt="Tenis"
-        />
-        <strong>Tenis muito Legal</strong>
-        <span>R$ 129,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-2-masculino/46/D16-4274-246/D16-4274-246_detalhe1.jpg?ts=1586370047?ims=280x280"
-          alt="Tenis"
-        />
-        <strong>Tenis muito Legal</strong>
-        <span>R$ 129,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+    const data = response.data.map((product) => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-2-masculino/46/D16-4274-246/D16-4274-246_detalhe1.jpg?ts=1586370047?ims=280x280"
-          alt="Tenis"
-        />
-        <strong>Tenis muito Legal</strong>
-        <span>R$ 129,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+    this.setState({ products: data });
+  }
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-hawk-2-masculino/46/D16-4274-246/D16-4274-246_detalhe1.jpg?ts=1586370047?ims=280x280"
-          alt="Tenis"
-        />
-        <strong>Tenis muito Legal</strong>
-        <span>R$ 129,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+  render() {
+    const { products } = this.state;
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+    return (
+      <ProductList>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
+              </div>
+
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
